119e046e0769ff5f53b496cb4c498821d0755e97
Silva Nelson <na.98abrahn@gmail.com>
2026-04-17 20:09:38 -0300

    palabra clave

--- funciones.h
+++ funciones.h
@@ -3,21 +3,17 @@
 #include <stdio.h>
 #include <string.h>
 
-void BuscarNombre(char **nombres, int id, int cant) {
-    if (id < 0 || id > cant)
-    {
-        printf("No se encuentra el nombre\n");
-    }else
-    {
-        printf("%s\n", nombres[id]);
-    }
-}
 
 int BuscarNombre(char **nombres, char palabra[], int cant) {
     for (int i = 0; i < cant; i++) {
-        if (strstr(nombres[i], palabra) != NULL) return i;
+        if (strstr(nombres[i], palabra) != NULL)
+        {
+            return i;
+        }else
+        {
+            return -1;   
+        }       
     }
-    return -1;
 }
 
 #endif
